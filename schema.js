81e4d39aa0921d51b7fc73ceada409b7d338839b
jjyepez <jjyepez@gmail.com>
2018-01-14 16:19:33 -0400

    Ampliado el eschema de la Demo

--- schema.js
+++ schema.js
@@ -6,13 +6,40 @@ const { makeExecutableSchema } = require('graphql-tools')
 // --- Definición del Schema GraphQL
 // --- obligatoriamente debe existir un type llamado Query (root) en el schema
 const typeDefs = `
-  type Curso{
-    id    : ID!
-    titulo: String!
+  # -- Query es la base (root) de todo el esquema y es obligatorio
+  type Query {
+    cursos     : [Curso]
+    profesores : [Profesor]
+    curso   ( id: Int ): Curso
+    profesor( id: Int ): Profesor
   }
 
-  type Query {
-    cursos: [Curso]
+  type Curso {
+    # -- Los campos con ! son requeridos
+    id          : ID!
+    titulo      : String!
+    descripcion : String!
+    profesor    : [Profesor]
+    rating      : Float
+    comentarios : [Comentario]
+  }
+
+  type Profesor {
+    id          : ID!
+    nombre      : String!
+    nacionalidad: String!
+    genero      : Genero
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO
+  }
+
+  type Comentario {
+    id    : ID!
+    nombre: String!
+    cuerpo: String!
   }
 `
 
